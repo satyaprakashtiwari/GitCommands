d5ee717e6dba65f66c158b47fab75e7ffba2cf6c
satyaprakashtiwari <satya.automationengg@gmail.com>
2020-02-01 18:05:44 +0530

    added lines in B

--- GitCommands/src/ClassB.java
+++ GitCommands/src/ClassB.java
@@ -5,6 +5,8 @@ public class ClassB {
 		// TODO Auto-generated method stub
 		
 		System.out.println("in class B");
+		System.out.println("added line 1");
+		System.out.println("added line 2");
 
 	}
 
