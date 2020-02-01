2e45e54c3475e46c5e9202ed4826b3595342f3cb
satyaprakashtiwari <satya.automationengg@gmail.com>
2020-02-01 18:11:18 +0530

    added class3

---
+++ GitCommands/src/ClassC.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author satyatiw
+ *
+ */
+public class ClassC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class C");
+
+	}
+
+}
